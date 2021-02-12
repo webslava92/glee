3cb6c5be5025135fc16329ef88a6a0099628e390
webslava92 <pustovit.slava@gmail.com>
2021-02-12 09:25:23 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,51 @@
-$(function(){
+$(function () {
+
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.product-item__btn').on('click', function () {
+    $('.product-item__btn').removeClass('product-item__btn--active');
+    $(this).addClass('product-item__btn--active');
+  });
+
+  $('.product-item__btn').on('click', function () {
+    $('.product-item__controls').removeClass('product-item__controls-visible');
+    $('.product-item__controls').addClass('product-item__controls-visible');
+  });
+
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#cccccc",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 700,
+    from: 100,
+    to: 500,
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    }
+  });
   
   $('.top-slider__inner').slick({
     dots: true,
